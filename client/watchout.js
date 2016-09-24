01f7e8e7159891fe893590742052b40ef181d46b
szhou1 <zhousteve3@gmail.com>
2016-09-23 18:56:39 -0700

    Completed dragging player

--- client/watchout.js
+++ client/watchout.js
@@ -1,13 +1,4 @@
-// start slingin' some d3 here.
-var svg = d3.select('svg');
-var width = +svg.attr('width');
-var height = +svg.attr('height');
-var enemyCount = 30;
-
-var gameStats = {
-  score: 0,
-  bestscore: 0
-};
+var enemyCount = 5;
 
 var createEnemies = function() {
   return d3.range(enemyCount).map(function(i) {
@@ -24,40 +15,57 @@ var render = function(enemyData) {
   var enemies = svg.selectAll('circle.enemy')
             .data(enemyData, function(enemy) { return enemy.id; });
 
-  // enemies = svg.selectAll('.circle')
-  //     .data(enemyData) //, function(d) { return d.id; }
   enemies.enter().append('circle')
       .attr('class', 'enemy')
-      // .attr('id', function(enemy) { return enemy.id; })
       .attr('cx', function(enemy) { return enemy.x; })
       .attr('cy', function(enemy) { return enemy.y; })
       .attr('r', 10);
-
 
   enemies
     .transition().duration(750).ease('linear')
     .attr('cx', function(enemy) { return enemy.x; } )
     .attr('cy', function(enemy) { return enemy.y; } );
-        
-
-  console.log(enemies);
 };
 
 var play = function() {
+  
   var gameTurn = function() {
     var newEnemyPositions = createEnemies();
     render(newEnemyPositions);
+
   };
 
   gameTurn();
   setInterval(gameTurn, 1000);
 };
 
+var width = 700,
+    height = 350,
+    radius = 20;
+
+var drag = d3.behavior.drag()
+    .origin(function(d) { return d; })
+    .on('drag', dragmove);
+
+var svg = d3.select('body').append('div').selectAll('svg')
+    .data(d3.range(1).map(function() { return {x: width / 2, y: height / 2}; }))
+  .enter().append('svg')
+    .attr('width', width)
+    .attr('height', height);
+
+svg.append('circle')
+    .attr('r', radius)
+    .attr('fill', 'orange')
+    .attr('cx', function(d) { return d.x; })
+    .attr('cy', function(d) { return d.y; })
+    .call(drag);
+
+function dragmove(d) {
+  d3.select(this)
+      .attr('cx', d.x = Math.max(radius, Math.min(width - radius, d3.event.x)))
+      .attr('cy', d.y = Math.max(radius, Math.min(height - radius, d3.event.y)));
+}
+
 play();
-
-
-
-
-
 
 
